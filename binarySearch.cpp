67e23e4a85bf2b8396ee3aa2adca77c8d3b546be
ZahwaNabila <zahwanabila434@gmail.com>
2026-04-14 10:56:15 +0700

    commit9

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -49,3 +49,20 @@ void bubbleSortarray()
         pass++;
     } while (pass <= nPanjang - 1);
 }
+
+void display()
+{
+    cout << "\n=======================================\n";
+    cout << "  Element Array Setelah Di Urutkan (Asc)\n";
+     cout << "\n====================================\n";
+
+     for (int j = 0; j < nPanjang; j++)
+     {
+        cout << element [j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+     }
+     cout << endl;
+}
