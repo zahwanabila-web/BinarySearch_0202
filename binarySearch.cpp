a4b8895bf384dc92563371fad77204d5c1e2f9e0
ZahwaNabila <zahwanabila434@gmail.com>
2026-04-14 10:31:54 +0700

    commit3

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -11,5 +11,13 @@ void input()
     {
         cout << "Masukkan banyaknya element pada array(maksimal 10): ";
         cin >> nPanjang;
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah element tidak boleh lebih dari 10. Silakan coba lagi.\n";
+        }
     }
 }
