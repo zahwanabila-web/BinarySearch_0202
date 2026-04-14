7d672932588388f3a196671dc65f734b8e33c23d
ZahwaNabila <zahwanabila434@gmail.com>
2026-04-14 10:28:27 +0700

    commit2

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -3,3 +3,13 @@ using namespace std;
 
 int element[10];
 int nPanjang;
+int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya element pada array(maksimal 10): ";
+        cin >> nPanjang;
+    }
+}
